149479f3e354a7a5ded0341aa2716a598e3de8c1
smammc <smammc@hotmail.com>
2025-07-08 18:12:23 +0100

    correction for deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/bestmen" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/bestmen/" : "",
   images: {
     unoptimized: true,
   },
